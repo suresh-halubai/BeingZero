9649a5f1e866e860aba61b9397e60feef034d7c6
Suresh Halubai <sureshhe311@gmail.com>
2021-11-01 22:31:22 -0500

    added few lines of code

--- src/main/java/com/beingzero/org/CheckBit.java
+++ src/main/java/com/beingzero/org/CheckBit.java
@@ -1,2 +1,33 @@
-package com.beingzero.org;public class CheckBit {
+package com.beingzero.org;
+
+import java.util.Scanner;
+
+public class CheckBit {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        DecToBinary(num);
+
+    }
+
+    public static void  DecToBinary(int num) {
+            int[] binNum = new int[32];
+            int i =0;
+
+            while(num >0){
+                binNum[i] = num % 2;
+                i++;
+                num = num/2;
+            }
+
+            /*for (int j = i-1; j >=0 ; j--) {
+
+                System.out.print(binNum[j]);
+            }*/
+
+            if(binNum[i-1] == 1)
+                System.out.println("true");
+
+    }
 }
--- src/main/java/com/beingzero/org/DigitSum.java
+++ src/main/java/com/beingzero/org/DigitSum.java
@@ -1,2 +1,22 @@
-package com.beingzero.org;public class DigitSum {
+package com.beingzero.org;
+
+import java.util.Scanner;
+
+public class DigitSum {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        System.out.println(findDigitSum(num));
+    }
+
+    public static int findDigitSum(int num) {
+        int sum = 0;
+        while(num != 0){
+            sum = sum+ num%10;
+            num = num/10;
+        }
+        return sum;
+    }
 }
