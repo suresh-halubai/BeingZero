1e2b74f0256e17ad4a48633a6a4ddb6e838beea1
Suresh Halubai <sureshhe311@gmail.com>
2021-11-30 22:31:43 -0600

    Find count of even number of Digits: https://leetcode.com/explore/learn/card/fun-with-arrays/521/introduction/3237/

---
+++ src/main/java/com/beingzero/org/FindNumbersWithEvenNumberOfDigits.java
@@ -0,0 +1,25 @@
+package com.beingzero.org;
+
+public class FindNumbersWithEvenNumberOfDigits {
+    public static int findNumbers(int[] nums) {
+        int count = 0;
+        int evenDigits = 0;
+
+        for(int i = 0; i < nums.length ; i++){
+            while(nums[i] != 0){
+                count++;
+                nums[i] = nums[i]/10;
+            }
+            if(count % 2 == 0 ){
+                evenDigits++;
+            }
+            count = 0;
+        }
+        return evenDigits;
+    }
+
+    public static void main(String[] args) {
+        int nums[] = {12,3,345,24,12};
+        System.out.println(findNumbers(nums));
+    }
+}
