43ece96142b6e15b6e6c8d50a4d73626c8df2614
Suresh Halubai <sureshhe311@gmail.com>
2021-11-30 22:02:42 -0600

    Find Max Occurence Of consecutive 1's:https://leetcode.com/explore/learn/card/fun-with-arrays/521/introduction/3238/

---
+++ src/main/java/com/beingzero/org/FindOccurenceOfConsecutiveOnes.java
@@ -0,0 +1,24 @@
+package com.beingzero.org;
+
+public class FindOccurenceOfConsecutiveOnes {
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0;
+        int max =0;
+
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == 1){
+                count++;
+                max = (max > count) ? max : count;
+            }
+            else
+            {
+                count = 0;
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,1,0,1,1,1,1,1,1,1,0,1,1,1,1,1};
+        System.out.println(findMaxConsecutiveOnes(nums));
+    }
+}
