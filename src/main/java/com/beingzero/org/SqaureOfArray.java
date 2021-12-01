9022c52e7b8b9d8bf4530958814122e1852fd543
Suresh Halubai <sureshhe311@gmail.com>
2021-11-30 22:50:33 -0600

    Sorted square of an array: https://leetcode.com/explore/learn/card/fun-with-arrays/521/introduction/3240/

---
+++ src/main/java/com/beingzero/org/SqaureOfArray.java
@@ -0,0 +1,23 @@
+package com.beingzero.org;
+
+import java.util.Arrays;
+
+public class SqaureOfArray {
+
+    public static int[] sortedSquares(int[] nums) {
+        for(int i = 0; i < nums.length ; i++){
+            nums[i] = nums[i] * nums[i];
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-4,-1,0,3,10};
+        int[] result = sortedSquares(nums);
+        for (int i = 0; i < nums.length ; i++) {
+            System.out.println(result[i]);
+        }
+
+    }
+}
